a68c60261c7bf8f548d6ec98082d4791e684a6f9
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-20 12:09:41 +0200

    Update settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'profiles.User'v
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
