b32231a59d2e75aa63eebecbd8ebe49d5aa183ef
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-21 12:39:23 +0200

    Update urls.py

--- server/profiles/urls.py
+++ server/profiles/urls.py
@@ -2,11 +2,27 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginPage, name="login"),
-    path('logout/', views.logoutUser, name="logout"),
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='profiles/login.html',
+             authentication_form=forms.BootstrapAuthenticationForm,
+             extra_context=
+             {
+                 'title': 'Log in',
+                 'year' : datetime.now().year,
+             }
+         ),
+         name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    
+    # path('login/', views.loginPage, name="login"),
+    # path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
-
-    path('', views.home, name="home"),
+    
     path("post/<str:pk>/", views.post, name="post"),
     path("profile/<str:pk>/", views.userProfile, name="user-profile"),
 
