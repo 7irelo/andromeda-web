0839124d5339c6e25a6a71ce2acd87c6214b8a0d
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-21 14:52:33 +0200

    Update views.py

--- server/profiles/views.py
+++ server/profiles/views.py
@@ -44,6 +44,19 @@ class LoginView(APIView):
         }
         return response
 
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256']
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated')
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
 @api_view(['GET'])
 def loginPage(request):
     page = "login"
