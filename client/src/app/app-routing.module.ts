1cdb10d639a225ce2fa8b1d2fa5608dce76566ec
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 01:38:13 +0200

    Update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
+    ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
