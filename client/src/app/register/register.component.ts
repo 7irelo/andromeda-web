2ee8704851672c343bfbf300c01a9bb5d69f617a
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 01:50:01 +0200

    Update register.component.ts

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,1 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router'
 
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+  constructor(
+      private formBuilder: FormBuilder,
+      private http: HttpClient,
+      private router: Router
+      ) {}
+
+  ngOnInit(): void {
+      this.form = this.formBuilder.group({
+          name: '',
+          surname: '',
+          email: '',
+          password: ''
+      });
+  }
+  
+  submit(): void {
+      this.http.post('http://localhost:5000/api/register', this.form.getRawValue())
+      .subscribe(() => this.router.navigate(['/login']));
+  }
+}
