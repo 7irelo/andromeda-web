c4436ad6f0be350f91b09bea4a1cafaa9ad4fb58
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 13:29:25 +0200

    Create models.py

---
+++ server/marketplace/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+# from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class Item(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    text = models.CharField(max_length=50)
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-updated", "-created"]
+
+    def __str__(self):
+        return self.text
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    text = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-updated", "-created"]
+
+    def __str__(self):
+        return self.text[0:50]
