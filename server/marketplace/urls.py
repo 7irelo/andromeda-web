02e52cf75a91ad6c133c5a9ef2a99028e11d5aa4
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 15:19:52 +0200

    Update urls.py

--- server/marketplace/urls.py
+++ server/marketplace/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from . import views
-from views import MarketplaceView, ItemView
+from .views import MarketplaceView, ItemView
 
 urlpatterns = [
-    path('', MarketplaceView.as_view(), name='marketplace'),
-    path("item/<str:pk>/", ItemView.as_view(), name="item"),
+    path('', MarketplaceView.as_view(), name='marketplace'),  # Register MarketplaceView
+    path('item/<str:pk>/', ItemView.as_view(), name='item'),    # Register ItemView with dynamic item ID
 ]
