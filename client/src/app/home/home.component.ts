a9fa9347aa221a0c98b938a961016acf265292f6
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 02:00:13 +0200

    Create home.component.ts

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Emitters } from '../emitters/emitters';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  message = ''
+  constructor(
+      private formBuilder: FormBuilder,
+      private http: HttpClient,
+      private router: Router
+      ) {}
+
+  ngOnInit(): void {
+      this.http.get('http://localhost:5000/api/user', { withCredentials:true }).subscribe(
+          (res: any) => {
+              this.message = `Hello ${res.user}`
+              Emitters.authEmmitter.emit(true);
+          }
+          err => {
+              this.message = "You are not logged in"
+              Emitters.authEmmitter.emit(false);
+          }
+          );
+      this.form = this.formBuilder.group({
+          name: '',
+          surname: '',
+          email: '',
+          password: ''
+      });
+  }
+  
+  submit(): void {
+      this.http.post('http://localhost:5000/api/register', this.form.getRawValue())
+      .subscribe(() => this.router.navigate(['/login']));
+  }
+}
+
