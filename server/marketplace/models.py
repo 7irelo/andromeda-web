84847b99efc470b6f7914ab2fb7fa3019917aa84
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 13:37:35 +0200

    Update models.py

--- server/marketplace/models.py
+++ server/marketplace/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-# from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
+from app.models import User
 
 class Item(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -15,7 +14,7 @@ class Item(models.Model):
     def __str__(self):
         return self.text
 
-class Comment(models.Model):
+class ItemComment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     text = models.TextField()
