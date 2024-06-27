15cfdeb6a40ad1c593e7f93193387aab893d0377
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 16:46:13 +0200

    Update views.py

--- server/app/views.py
+++ server/app/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
-from rest_framework.decorators import api_view
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
-from rest_framework.Views import APIView
+from rest_framework.views import APIView
 from .models import Post, Comment, User
 from .forms import PostForm, UserForm, MyUserCreationForm
-from .serializers import UserSerializer
+from .serializers import UserSerializer, PostSerializer, CommentSerializer
 import jwt, datetime
 
 class RegisterView(APIView):
@@ -21,27 +18,23 @@ class RegisterView(APIView):
 
 class LoginView(APIView):
     def post(self, request):
-        email = request.data['email']
-        password = request.data['password']
-        user = User.objects.filter(email=email)
-        if user is None:
-            raise AuthenticationFailed('User not found')
-        if not user.check_password(password):
-            raise AuthenticationFailed('Incorrect password')
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = User.objects.filter(email=email).first()
+        if not user or not user.check_password(password):
+            raise AuthenticationFailed('Invalid credentials')
+
         payload = {
             'id': user.id,
             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
             'iat': datetime.datetime.utcnow()
         }
-        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
         response = Response()
         response.set_cookie(key='jwt', value=token, httponly=True)
-        response.data = {
-            'jwt': token
-        }
+        response.data = {'jwt': token}
         return response
 
 class UserView(APIView):
@@ -49,51 +42,41 @@ class UserView(APIView):
         token = request.COOKIES.get('jwt')
         if not token:
             raise AuthenticationFailed('Unauthenticated')
+
         try:
-            payload = jwt.decode(token, 'secret', algorithm=['HS256']
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
             raise AuthenticationFailed('Unauthenticated')
+
         user = User.objects.filter(id=payload['id']).first()
-        
-        posts = user.post_set.all()
-        comments = user.comment_set.all()
-        context = {"user": user, "posts": posts, "comments": comments}
         serializer = UserSerializer(user)
         return Response(serializer.data)
-class UpdateUser(APIView):
+
+class UpdateUserView(APIView):
     def get(self, request):
         user = request.user
         form = UserForm(instance=user)
-        context = {"form": form}
-        return render(request, "profile/update_user.html", context)
+        return render(request, "profile/update_user.html", {"form": form})
     
     def post(self, request):
+        user = request.user
         form = UserForm(request.POST, instance=user)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
             return redirect("user-profile", pk=user.id)
+        return render(request, "profile/update_user.html", {"form": form})
 
-        context = {"form": form}
-        return render(request, "profile/update_user.html", context)
-        
 class LogoutView(APIView):
     def post(self, request):
         response = Response()
         response.delete_cookie('jwt')
-        response.data = {
-            'message': success
-        }
+        response.data = {'message': 'success'}
+        return response
 
 class HomeView(APIView):
     def get(self, request):
-        q = request.GET.get("q") if request.GET.get("q") != None else ""
-
+        q = request.GET.get("q", "")
         posts = Post.objects.filter(Q(user__name__icontains=q) | Q(text__icontains=q))
-  
-        post_count = post.count()
-        comments = Comment.objects.filter(Q(post___text___icontains=q))
-
-        context = {"posts": posts, "post_count": post_count, "comments": comments}
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
 
@@ -102,66 +85,57 @@ class PostView(APIView):
         post = Post.objects.get(id=pk)
         comments = post.comment_set.all().order_by("created")
         participants = post.participants.all()
-        context = {"post": post, "comments": comments, "participants": participants}
-        serializer = PostSerializer(post, many=False)
+        serializer = PostSerializer(post)
         return Response(serializer.data)
 
     def post(self, request, pk):
         post = Post.objects.get(id=pk)
-        comments = Comment.objects.create(
+        Comment.objects.create(
             user=request.user,
             post=post,
-            text=request.POST.get("text")
+            text=request.data.get("text")
         )
         post.participants.add(request.user)
         return redirect("post", pk=post.id)
-class CreatePost(APIView):
+
+class CreatePostView(APIView):
     def get(self, request):
         form = PostForm()
-        context = {"form": form}
-        serializer = PostSerializer(post, many=False)
-        return Response(serializer.data)
+        return render(request, "posts/create_post.html", {"form": form})
         
     def post(self, request):
-        post_text = request.POST.get("text")
-        Post.objects.create(
-            host=request.user,
-            text=post_text,
-        )
+        post_text = request.data.get("text")
+        Post.objects.create(host=request.user, text=post_text)
         return redirect("home")
 
-class UpdatePost(APIView):
-    def get(request, pk):
+class UpdatePostView(APIView):
+    def get(self, request, pk):
         post = Post.objects.get(id=pk)
         form = PostForm(instance=post)
-        context = {"form": form, "topics": topics, "post":post}
-        serializer = PostSerializer(post, many=False)
-        return Response(serializer.data)
+        return render(request, "posts/update_post.html", {"form": form, "post": post})
         
     def post(self, request, pk):
         post = Post.objects.get(id=pk)
-        post.text = request.POST.get("text")
+        post.text = request.data.get("text")
         post.save()
         return redirect("home")
 
-class DeletePost(APIView):
-    def get(request, pk):
+class DeletePostView(APIView):
+    def get(self, request, pk):
         post = Post.objects.get(id=pk)
-        serializer = PostSerializer(post, many=False)
-        return Response(serializer.data)
+        return render(request, "posts/delete_post.html", {"post": post})
         
-    def post(self, request):
+    def post(self, request, pk):
         post = Post.objects.get(id=pk)
         post.delete()
         return redirect("home")
 
-class DeleteComment(APIView):
-    def get(request, pk):
+class DeleteCommentView(APIView):
+    def get(self, request, pk):
         comment = Comment.objects.get(id=pk)
-        serializer = CommentSerializer(comment, many=False)
-        return Response(serializer.data)
+        return render(request, "comments/delete_comment.html", {"comment": comment})
 
-    def post(self, request):
+    def post(self, request, pk):
         comment = Comment.objects.get(id=pk)
         comment.delete()
         return redirect("home")
