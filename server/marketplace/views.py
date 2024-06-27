3e1337341d01f990bbdd90766855c9e54165f779
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 18:23:32 +0200

    Update views.py

--- server/marketplace/views.py
+++ server/marketplace/views.py
@@ -4,7 +4,6 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Item, ItemComment
-from .forms import PostForm, UserForm, MyUserCreationForm
 from .serializers import ItemSerializer
 import jwt, datetime
 
