2e34ad3d393cd372108e12192aa1a61133528cc5
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 17:41:47 +0200

    Update views.py

--- server/posts/views.py
+++ server/posts/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate, login, logout
-from django.db.models import Q
-from rest_framework.exceptions import AuthenticationFailed
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Post, Comment, User
-from .forms import PostForm, UserForm, MyUserCreationForm
+from rest_framework import status
+from .models import Post, Comment
 from .serializers import UserSerializer, PostSerializer, CommentSerializer
-import jwt, datetime
+from .forms import PostForm, UserForm, MyUserCreationForm
 
 class PostView(APIView):
     def get(self, request, pk):
@@ -15,7 +12,7 @@ class PostView(APIView):
         comments = post.comment_set.all().order_by("created")
         participants = post.participants.all()
         serializer = PostSerializer(post)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, pk):
         post = get_object_or_404(Post, id=pk)
@@ -25,46 +22,32 @@ class PostView(APIView):
             text=request.data.get("text")
         )
         post.participants.add(request.user)
-        return redirect("post", pk=post.id)
-
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
 class CreatePostView(APIView):
-    def get(self, request):
-        form = PostForm()
-        return render(request, "posts/create_post.html", {"form": form})
-
     def post(self, request):
         post_text = request.data.get("text")
-        Post.objects.create(host=request.user, text=post_text)
-        return redirect("home")
+        post = Post.objects.create(host=request.user, text=post_text)
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class UpdatePostView(APIView):
-    def get(self, request, pk):
-        post = get_object_or_404(Post, id=pk)
-        form = PostForm(instance=post)
-        return render(request, "posts/update_post.html", {"form": form, "post": post})
-
-    def post(self, request, pk):
+    def put(self, request, pk):
         post = get_object_or_404(Post, id=pk)
         post.text = request.data.get("text")
         post.save()
-        return redirect("home")
-
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
 class DeletePostView(APIView):
-    def get(self, request, pk):
-        post = get_object_or_404(Post, id=pk)
-        return render(request, "posts/delete_post.html", {"post": post})
-
-    def post(self, request, pk):
+    def delete(self, request, pk):
         post = get_object_or_404(Post, id=pk)
         post.delete()
-        return redirect("home")
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class DeleteCommentView(APIView):
-    def get(self, request, pk):
-        comment = get_object_or_404(Comment, id=pk)
-        return render(request, "comments/delete_comment.html", {"comment": comment})
-
-    def post(self, request, pk):
+    def delete(self, request, pk):
         comment = get_object_or_404(Comment, id=pk)
         comment.delete()
-        return redirect("home")
+        return Response(status=status.HTTP_204_NO_CONTENT)
