0531ab24d57c89ea66f85c8de76aa507ae3ea0e2
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 17:03:27 +0200

    Update urls.py

--- server/app/urls.py
+++ server/app/urls.py
@@ -13,11 +13,4 @@ urlpatterns = [
     # profile
     path('<str:pk>/', UserView.as_view(), name='profile'),
     path('update-user/', UpdateUserView.as_view(), name='update-user'),
-
-    # posts
-    path('post/<str:pk>/', PostView.as_view(), name='post'),
-    path('create-post/', CreatePostView.as_view(), name='create-post'),
-    path('update-post/<str:pk>/', UpdatePostView.as_view(), name='update-post'),
-    path('delete-post/<str:pk>/', DeletePostView.as_view(), name='delete-post'),
-    path('delete-comment/<str:pk>/', DeleteCommentView.as_view(), name='delete-comment'),
 ]
