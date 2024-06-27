adf488913bc5c00c7242cb0c800c5d5d74d4f006
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 12:18:32 +0200

    Update urls.py

--- server/server/urls.py
+++ server/server/urls.py
@@ -12,6 +12,13 @@ from app import forms, views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('profiles.urls')),
+    path('api/', include('app.urls')),
+    path('api/friends', include('friends.urls')),
+    path('api/messages', include('messages.urls')),
+    path('api/watch', include('watch.urls')),
+    path('api/notifications', include('notifications.urls')),
+    path('api/marketplace', include('marketplace.urls')),
+    path('api/groups', include('groups.urls')),
+    path('api/pages', include('pages.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
