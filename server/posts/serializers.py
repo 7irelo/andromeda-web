f063d53b9c0b28815e5468428fd006e908942b2a
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-27 17:56:00 +0200

    Create serializers.py

---
+++ server/posts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'host', 'text', 'participants', 'updated', 'created']
+        read_only_fields = ['id', 'updated', 'created']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'post', 'text', 'updated', 'created']
+        read_only_fields = ['id', 'updated', 'created']
