751d829e4ff68c9b21018e8cb413dbfba99bd49b
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-28 11:22:14 +0200

    Update serializers.py

--- server/app/serializers.py
+++ server/app/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'password']
+        fields = ['username', 'name', 'surname', 'bio', 'email', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
