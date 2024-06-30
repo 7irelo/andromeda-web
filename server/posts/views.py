426e5e03e2c2df2cedc945ab630311bc562cd037
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-30 03:32:36 +0200

    Update views.py

--- server/posts/views.py
+++ server/posts/views.py
@@ -7,16 +7,16 @@ from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 class PostsView(APIView):
-    def get(self, request):
+    async def get(self, request):
         query = request.GET.get("q", "")
-        posts = Post.objects.filter(Q(host__username__icontains=query) | Q(text__icontains=query))
+        posts = await Post.objects.filter(Q(host__username__icontains=query) | Q(text__icontains=query)).all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class PostView(APIView):
-    def get(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        comments = Comment.objects.filter(post=post).order_by("created")
+    async def get(self, request, pk):
+        post = await get_object_or_404(Post, pk=pk)
+        comments = await Comment.objects.filter(post=post).order_by("created").all()
         post_serializer = PostSerializer(post)
         comments_serializer = CommentSerializer(comments, many=True)
         return Response({
@@ -24,14 +24,14 @@ class PostView(APIView):
             "comments": comments_serializer.data
         }, status=status.HTTP_200_OK)
 
-    def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        comment = Comment.objects.create(
+    async def post(self, request, pk):
+        post = await get_object_or_404(Post, pk=pk)
+        comment = await Comment.objects.create(
             user=request.user,
             post=post,
             text=request.data.get("text")
         )
-        post.participants.add(request.user)
+        await post.participants.add(request.user)
         post_serializer = PostSerializer(post)
         comment_serializer = CommentSerializer(comment)
         return Response({
@@ -40,30 +40,30 @@ class PostView(APIView):
         }, status=status.HTTP_201_CREATED)
 
 class CreatePostView(APIView):
-    def post(self, request):
+    async def post(self, request):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(host=request.user)
+            await serializer.save(host=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UpdatePostView(APIView):
-    def put(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+    async def put(self, request, pk):
+        post = await get_object_or_404(Post, pk=pk)
         serializer = PostSerializer(post, data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.save()
+            await serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class DeletePostView(APIView):
-    def delete(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        post.delete()
+    async def delete(self, request, pk):
+        post = await get_object_or_404(Post, pk=pk)
+        await post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class DeleteCommentView(APIView):
-    def delete(self, request, pk):
-        comment = get_object_or_404(Comment, pk=pk)
-        comment.delete()
+    async def delete(self, request, pk):
+        comment = await get_object_or_404(Comment, pk=pk)
+        await comment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
