e38eb2245898709a28b22c65faaa67d7d98380e5
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-01 12:27:23 +0200

    Update nav.component.ts

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,27 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Emitters } from '../emitters/emitters';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
   authenticated = false;
-  constructor(private http: HttpClient) {}
+  searchForm: FormGroup;
+
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+    private searchService: SearchService
+  ) {
+    this.searchForm = this.formBuilder.group({
+      query: ['']
+    });
+  }
 
   ngOnInit(): void {
-      Emitters.authEmmitter.subscribe(
-          (auth: boolean) => {
-              this.authenticated = auth;
-          }
-      );
+    Emitters.authEmitter.subscribe(
+      (auth: boolean) => {
+        this.authenticated = auth;
+      }
+    );
   }
   
-  logout() : void {
-      this.http.post('http://localhost:8000/api/logout', {}, { withCredsntials: true })
+  logout(): void {
+    this.http.post('http://localhost:8000/api/logout', {}, { withCredentials: true })
       .subscribe(() => this.authenticated = false);
   }
 
+  onSearch(): void {
+    const query = this.searchForm.get('query')?.value || '';
+    this.searchService.changeQuery(query);
+  }
 }
