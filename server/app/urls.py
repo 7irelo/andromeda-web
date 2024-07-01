dfbf7b86fc5958014d68530c3260f4c581221a47
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-01 18:17:50 +0200

    Update urls.py

--- server/app/urls.py
+++ server/app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('users/<str:username>/', UserView.as_view(), name='user-detail'),
     path('home/', HomeView.as_view(), name='home'),
     # Additional paths for other views if needed
 ]
