bb13022a12e1eda6f2b00c7c76247c485bfedf1e
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-01 15:21:47 +0200

    Update views.py

--- server/app/views.py
+++ server/app/views.py
@@ -39,36 +39,6 @@ class LoginView(APIView):
         response.data = {'jwt': token}
         return response
 
-class UserView(APIView):
-    def get(self, request):
-        token = request.COOKIES.get('jwt')
-        if not token:
-            raise AuthenticationFailed('Unauthenticated')
-
-        try:
-            payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=['HS256'])
-            user = User.objects.get(username=payload['username'])
-        except (jwt.ExpiredSignatureError, jwt.DecodeError):
-            raise AuthenticationFailed('Unauthenticated')
-        except User.DoesNotExist:
-            raise AuthenticationFailed('User not found')
-
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-class UpdateUserView(APIView):
-    def get(self, request):
-        user = request.user
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-    def put(self, request):
-        user = request.user
-        serializer = UserSerializer(user, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
 class LogoutView(APIView):
     def post(self, request):
         response = Response()
