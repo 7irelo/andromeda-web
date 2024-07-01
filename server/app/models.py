2846fda2d84d6d1087546cbb2c9364c46442b958
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-01 17:03:34 +0200

    Update models.py

--- server/app/models.py
+++ server/app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     username = models.CharField(max_length=150, unique=True, primary_key=True)
     email = models.EmailField(unique=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True, default="profile.png")
 
