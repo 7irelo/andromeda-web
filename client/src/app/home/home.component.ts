440bf9d88e324dc919f8bfbb059420b130ed431e
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-01 12:46:48 +0200

    Update home.component.ts

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PostService } from '../post.service';
 import { SearchService } from '../search.service';
 
@@ -10,11 +11,19 @@ import { SearchService } from '../search.service';
 export class HomeComponent implements OnInit {
   posts: any[] = [];
   message: string = '';
+  showPostForm: boolean = false;
+  postText: string = '';
+  postForm: FormGroup;
 
   constructor(
     private postService: PostService,
-    private searchService: SearchService
-  ) {}
+    private searchService: SearchService,
+    private formBuilder: FormBuilder
+  ) {
+    this.postForm = this.formBuilder.group({
+      text: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
     this.searchService.currentQuery.subscribe(query => {
@@ -34,5 +43,30 @@ export class HomeComponent implements OnInit {
         this.message = 'Error fetching posts';
       }
     );
+  }
+
+  togglePostForm(): void {
+    this.showPostForm = !this.showPostForm;
+  }
+
+  submitPost(): void {
+    if (this.postForm.valid) {
+      const postData = {
+        text: this.postForm.value.text
+      };
+
+      this.postService.createPost(postData).subscribe(
+        data => {
+          // Refresh posts after successful creation
+          this.loadPosts();
+          this.showPostForm = false; // Hide the form after successful submission
+          this.postForm.reset(); // Reset the form
+        },
+        error => {
+          console.error('Error creating post', error);
+          // Handle error if needed
+        }
+      );
+    }
   }
 }
