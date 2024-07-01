b7e9cbca6b09d88c5ad2b48b75e2e3c427d962af
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-01 07:14:24 +0200

    Update urls.py

--- server/posts/urls.py
+++ server/posts/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import PostsView, PostView, CreatePostView, UpdatePostView, DeletePostView, CommentView
 
 urlpatterns = [
-    path('posts/', PostsView.as_view(), name='posts-list'),
-    path('posts/create/', CreatePostView.as_view(), name='create-post'),
-    path('posts/<int:pk>/', PostView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/update/', UpdatePostView.as_view(), name='update-post'),
-    path('posts/<int:pk>/delete/', DeletePostView.as_view(), name='delete-post'),
-    path('posts/<int:post_pk>/comments/<int:pk>/', CommentView.as_view(), name='comment-detail'),
-    path('posts/<int:post_pk>/comments/', CommentView.as_view(), name='create-comment'),
+    path('/', PostsView.as_view(), name='posts-list'),
+    path('create/', CreatePostView.as_view(), name='create-post'),
+    path('<int:pk>/', PostView.as_view(), name='post-detail'),
+    path('<int:pk>/update/', UpdatePostView.as_view(), name='update-post'),
+    path('<int:pk>/delete/', DeletePostView.as_view(), name='delete-post'),
+    path('<int:post_pk>/comments/<int:pk>/', CommentView.as_view(), name='comment-detail'),
+    path('<int:post_pk>/comments/create-comment', CommentView.as_view(), name='create-comment'),
 ]
