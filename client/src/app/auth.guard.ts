88f586ec18dd6a2ac42bb0b5ee4c9b05aaec44b2
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 14:59:01 +0200

    Update auth.guard.ts

--- client/src/app/auth.guard.ts
+++ client/src/app/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +15,18 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> {
     return this.authService.checkAuth().pipe(
-      map(isAuthenticated => isAuthenticated || this.router.createUrlTree(['/login']))
+      map(isAuthenticated => {
+        if (isAuthenticated) {
+          return true;
+        } else {
+          console.log('Not authenticated - redirecting to login');
+          return this.router.createUrlTree(['/login']);
+        }
+      }),
+      catchError(error => {
+        console.error('Error checking authentication status', error);
+        return this.router.createUrlTree(['/login']);
+      })
     );
   }
 }
