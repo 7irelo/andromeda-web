aa0f4589f66af4b2e346594be32b94c02200923c
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 11:29:31 +0200

    Update recommendations.py

--- server/posts/recommendations.py
+++ server/posts/recommendations.py
@@ -1,13 +1,20 @@
 from .models import Post, User
 
 def get_recommended_posts(user: User):
-    # Get posts liked by the user
+    """
+    Get a list of recommended posts for the given user.
+
+    Recommendations are based on:
+    - Posts liked by users who have liked the same posts as the given user.
+    - Excludes posts already liked by the given user.
+    """
+    # Step 1: Get posts liked by the user
     liked_posts = Post.objects.filter(likes=user)
     
-    # Get users who liked the same posts
+    # Step 2: Get users who liked the same posts
     similar_users = User.objects.filter(post_likes__in=liked_posts).exclude(id=user.id).distinct()
 
-    # Get posts liked by similar users but not yet liked by the current user
+    # Step 3: Get posts liked by similar users but not yet liked by the current user
     recommended_posts = Post.objects.filter(likes__in=similar_users).exclude(likes=user).distinct()
 
     return recommended_posts
