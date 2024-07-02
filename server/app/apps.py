9544a7e55f0e4a3ea7f0a12740ee6d898753f41b
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 10:18:39 +0200

    Update apps.py

--- server/app/apps.py
+++ server/app/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+import logging
 
 class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+    verbose_name = 'Base Application'
+
+    def ready(self):
+        # Set up logging for the app
+        logging.basicConfig(
+            format='%(asctime)s %(levelname)s [%(name)s] %(message)s',
+            level=logging.INFO
+        )
+        
+        # Import and register signals
+        import app.signals
+
+        # Any other app-specific setup
