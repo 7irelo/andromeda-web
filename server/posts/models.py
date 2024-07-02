99985b62044c24af64961b14033be1d3095f806e
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 12:14:03 +0200

    Update models.py

--- server/posts/models.py
+++ server/posts/models.py
@@ -9,9 +9,10 @@ class Post(models.Model):
     Represents a post created by a user.
     """
     creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name="Creator")
-    content = models.TextField(max_length=50, verbose_name="Post Content")
+    content = models.TextField(verbose_name="Post Content")
     participants = models.ManyToManyField(User, related_name="post_participants", blank=True, verbose_name="Participants")
     likes = models.ManyToManyField(User, related_name="post_likes", blank=True, verbose_name="Likes")
+    tags = models.ManyToManyField('Tag', related_name='posts', blank=True, verbose_name="Tags")
     updated = models.DateTimeField(auto_now=True, verbose_name="Last Updated")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Date Created")
 
@@ -34,6 +35,18 @@ class Like(models.Model):
     def __str__(self):
         return f"Like by {self.user.username} on post {self.post.id}"
 
+@receiver(post_save, sender=Like)
+def create_like_notification(sender, instance, created, **kwargs):
+    """
+    Create a notification when a post is liked.
+    """
+    if created:
+        Notification.objects.create(
+            user=instance.post.creator,
+            post=instance.post,
+            message=f'{instance.user.username} liked your post.'
+        )
+
 class Comment(models.Model):
     """
     Represents a comment on a post by a user.
@@ -52,18 +65,6 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment by {self.user.username} on post {self.post.id}: {self.text[:50]}"
 
-@receiver(post_save, sender=Like)
-def create_like_notification(sender, instance, created, **kwargs):
-    """
-    Create a notification when a post is liked.
-    """
-    if created:
-        Notification.objects.create(
-            user=instance.post.creator,
-            post=instance.post,
-            message=f'{instance.user.username} liked your post.'
-        )
-
 @receiver(post_save, sender=Comment)
 def create_comment_notification(sender, instance, created, **kwargs):
     """
@@ -75,3 +76,12 @@ def create_comment_notification(sender, instance, created, **kwargs):
             post=instance.post,
             message=f'{instance.user.username} commented on your post.'
         )
+
+class Tag(models.Model):
+    """
+    Represents a tag that can be associated with a post.
+    """
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
