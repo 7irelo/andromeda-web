2c6bd81a1a76c4e83131b9c85d424f96ab520aaf
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 09:51:00 +0200

    Update views.py

--- server/users/views.py
+++ server/users/views.py
@@ -33,3 +33,6 @@ class UserView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class UsersView(APIView):
+    pass
