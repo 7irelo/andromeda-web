45d17746e17f20b0891e0478c08348f8f1512c8b
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 10:05:59 +0200

    Remove UserView

--- server/app/urls.py
+++ server/app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, UserView
+from .views import RegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
