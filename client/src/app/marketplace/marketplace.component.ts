c3b45a98f69122e3b37fa87de2e068c30feeedec
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 15:56:04 +0200

    Create marketplace.component.ts

---
+++ client/src/app/marketplace/marketplace.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { MarketplaceService } from '../services/marketplace.service';
+import { Product } from '../models/product.model';
+
+@Component({
+  selector: 'app-marketplace',
+  templateUrl: './marketplace.component.html',
+  styleUrls: ['./marketplace.component.css']
+})
+export class MarketplaceComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private marketplaceService: MarketplaceService) {}
+
+  ngOnInit(): void {
+    this.marketplaceService.getProducts().subscribe((products) => {
+      this.products = products;
+    });
+  }
+}
