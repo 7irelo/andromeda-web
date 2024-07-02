72f95033c08d501960122efa2a3f3765ab8c0d44
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 10:05:19 +0200

    Update urls.py

--- server/app/urls.py
+++ server/app/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, UserView, HomeView
+from .views import RegisterView, LoginView, LogoutView, UserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('home/', HomeView.as_view(), name='home'),
-    # Additional paths for other views if needed
 ]
-
