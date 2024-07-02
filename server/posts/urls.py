b1c61c680017fb0b2d8560eb4e61382c3fef71f1
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 13:31:45 +0200

    Update urls.py

--- server/posts/urls.py
+++ server/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostsView, PostView, CommentView
+from .views import PostsView, PostView, CommentView, RecommendedPostsView
 
 urlpatterns = [
-    path('', PostsView.as_view(), name='posts-list'),  # List and create posts
-    path('<int:pk>/', PostView.as_view(), name='post-detail'),  # Retrieve, update, and delete a specific post
-    path('<int:post_pk>/comments/', CommentView.as_view(), name='comments-list'),  # List and create comments on a specific post
-    path('<int:post_pk>/comments/<int:pk>/', CommentView.as_view(), name='comment-detail'),  # Retrieve, update, and delete a specific comment
+    path('', PostsView.as_view(), name='posts-list'),
+    path('recommended/', RecommendedPostsView.as_view(), name='recommended-posts'),
+    path('<int:pk>/', PostView.as_view(), name='post-detail'),
+    path('<int:post_pk>/comments/', CommentView.as_view(), name='comments-list'),
+    path('<int:post_pk>/comments/<int:pk>/', CommentView.as_view(), name='comment-detail'),
 ]
