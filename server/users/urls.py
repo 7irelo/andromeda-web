aa76d23ea609c886bee928fdc5ff2a19317f5849
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 10:36:25 +0200

    Update urls.py

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, FriendsView
+
+app_name = 'users'
 
 urlpatterns = [
     path('users/<str:username>/', UserView.as_view(), name='user-detail'),
+    path('users/<str:username>/friends/', FriendsView.as_view(), name='user-friends'),
 ]
