15ba85e439ea0cb81332c2fd5317177d411de8bc
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-02 12:11:16 +0200

    Update recommendations.py

--- server/posts/recommendations.py
+++ server/posts/recommendations.py
@@ -1,20 +1,18 @@
+from django.db.models import Count
 from .models import Post, User
 
 def get_recommended_posts(user: User):
-    """
-    Get a list of recommended posts for the given user.
-
-    Recommendations are based on:
-    - Posts liked by users who have liked the same posts as the given user.
-    - Excludes posts already liked by the given user.
-    """
-    # Step 1: Get posts liked by the user
     liked_posts = Post.objects.filter(likes=user)
-    
-    # Step 2: Get users who liked the same posts
     similar_users = User.objects.filter(post_likes__in=liked_posts).exclude(id=user.id).distinct()
-
-    # Step 3: Get posts liked by similar users but not yet liked by the current user
     recommended_posts = Post.objects.filter(likes__in=similar_users).exclude(likes=user).distinct()
-
+    user_liked_tags = Post.objects.filter(likes=user).values_list('tags', flat=True)
+    
+    if user_liked_tags:
+        recommended_posts = recommended_posts.filter(tags__in=user_liked_tags).distinct()
+    user_friends = user.friends.all()
+   
+    if user_friends.exists():
+        recommended_posts = recommended_posts.filter(creator__in=user_friends).distinct()
+    recommended_posts = recommended_posts.annotate(num_likes=Count('likes')).order_by('-num_likes', '-created')[:10]
+   
     return recommended_posts
