1fc064d17c565a22948aaaef078dbeaed522bcf3
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-05 15:34:37 +0200

    Update apps.py

--- server/friends/apps.py
+++ server/friends/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-class FriendsConfig(AppConfig):
+class GroupsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'friends'
+    name = 'groups'
